a48379aa02003f942775aa123bea1a695b1ed4b2
Nikos Karampas <nikos.karampas@hotmail.com>
2023-04-04 17:25:49 +0300

    Added Employee requests, responses and DTO

---
+++ EmployeesAPI/Contracts/Data/EmployeeDto.cs
@@ -0,0 +1,13 @@
+namespace EmployeesAPI.Contracts.Data
+{
+    public class EmployeeDto
+    {
+        public string? Id { get; set; }
+
+        public string? Department { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Country { get; set; }
+    }
+}
---
+++ EmployeesAPI/Contracts/Requests/EmployeeRequest.cs
@@ -0,0 +1,11 @@
+namespace EmployeesAPI.Contracts.Requests
+{
+    public class EmployeeRequest
+    {
+        public string Department { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+    }
+}
